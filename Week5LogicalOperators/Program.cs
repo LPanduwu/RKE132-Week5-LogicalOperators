e6876b0e9b997261e74787775c108435d52efd89
LPanduwu <liisbeth.prangli@tktk.ee>
2023-05-29 17:57:15 +0300

    add Week5LogicalOperators code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,11 @@
+﻿//AND - &&
+//true && true (&& true) --> true
+//true && false --> false
+//false && true --> false
+//false && false --> flase
+
+//OR - || (pipes)
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
